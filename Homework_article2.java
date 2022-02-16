1b2e3c0d717373ec0cc949f023d28ee03832cefe
Benjarat Burapajetrak <85210845+geneyeans@users.noreply.github.com>
2022-02-16 15:06:40 +0700

    Add files via upload

---
+++ Homework_article2.java
@@ -0,0 +1,17 @@
+package homework1;
+
+public class Homework_article2 {
+	public static void main(String[] args) {
+		int s = 100;
+		System.out.println("This is a Int + s") ;
+		boolean b = true;
+		System.out.println("This is a boolean + b ") ;
+		String tiger = "Happy birthday";
+		System.out.println("This is a String + tiger ") ;
+		char ch1 = 'c' ;
+		System.out.println("This is a char + ch1 ") ;
+		short o = 5 ;
+		System.out.println("This is a short + o ") ;
+	}
+}
+		
