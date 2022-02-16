2d7d56290edd42f77a577062f8a49f83393bf82d
Benjarat Burapajetrak <85210845+geneyeans@users.noreply.github.com>
2022-02-16 15:06:06 +0700

    Add files via upload

---
+++ Homework_article1.java
@@ -0,0 +1,14 @@
+package homework1;
+
+public class Homework_article1 {
+	public static void main(String[] args) {
+		/* Data type: integer
+		   Variable name: s
+		 */
+		int s = 100;
+		//Assign value to s = 100
+		System.out.println("This is a Int = " + s) ;
+	}
+}
+
+
